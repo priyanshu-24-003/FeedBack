01f731267dd2180b525c9847dbc7056981bb5fe9
Priyanshu <priyanshu24003@gmail.com>
2026-08-02 22:28:49 +0530

    ++"

--- src/connections/credentials.py
+++ src/connections/credentials.py
@@ -11,11 +11,4 @@ class Credential():
     Secret_Key = "Aws_Secret_Key"
 
 
-    # URI = 'https://dagshub.com/priyanshu24003/FeedBack.mlflow'
-    # OWNER = 'priyanshu24003'
-    # PROJECT_NAME = 'FeedBack'
-    # S3_Bucket_Name = "feedback-project-data"
-    # Access_Key = "AKIA43F7NT3IKNYBVHTI"
-    # Secret_Key = "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"
-    # Console_password = "ZzZ28N[4"
-    # User_Name = "User_Learner"
+ 
