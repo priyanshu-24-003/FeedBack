082e1f937449c4b31024f6366c027465777610c9
Priyanshu <priyanshu24003@gmail.com>
2026-08-02 23:02:20 +0530

    citest-6-

--- src/connections/credentials.py
+++ src/connections/credentials.py
@@ -6,9 +6,9 @@ class Credential():
     URI = "Mlflow_URI"
     OWNER = "Owner"
     PROJECT_NAME = 'FeedBack'
-    S3_Bucket_Name = "S3BucketName"
-    Access_Key = "Aws_Access_Key"
-    Secret_Key = "Aws_Secret_Key"
+    S3_Bucket_Name = "S3BUCKETNAME"
+    Access_Key = "AWS_ACCESS_KEY"
+    Secret_Key = "AWS_SECRET_KEY"
 
 
  
