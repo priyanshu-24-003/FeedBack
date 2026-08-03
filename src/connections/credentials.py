e5db6eb42b47f54906c3bfd0b0c69f2ec8ff78d0
Priyanshu <priyanshu24003@gmail.com>
2026-08-03 18:52:38 +0530

    ++, [skip ci]

--- src/connections/credentials.py
+++ src/connections/credentials.py
@@ -9,6 +9,6 @@ class Credential():
     S3_Bucket_Name = "S3BUCKETNAME"
     Access_Key = "AWS_ACCESS_KEY"
     Secret_Key = "AWS_SECRET_KEY"
-    Dags_Token = "DAGSTOKEN"
+    Dags_Token = "DAGS_TOKEN"
 
  
