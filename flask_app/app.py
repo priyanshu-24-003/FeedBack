afe6321e0f83139f047a60d1632ce11489db8d15
Priyanshu <priyanshu24003@gmail.com>
2026-08-03 21:11:13 +0530

    CI_Test2: to test the flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -70,28 +70,19 @@ def normalize_text(text):
 
     return text
 
-# Below code block is for local use
-# -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri(Credential.URI)
-dagshub.init(repo_owner=Credential.OWNER, repo_name=Credential.PROJECT_NAME, mlflow=True)
-# -------------------------------------------------------------------------------------
+dagshub_token = os.getenv(Credential.Dags_Token)
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# Below code block is for production use
-# -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+dagshub_url = "https://dagshub.com"
+repo_owner = os.getenv(Credential.OWNER)
+repo_name = Credential.PROJECT_NAME
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
-# -------------------------------------------------------------------------------------
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 
 # Initialize Flask app
